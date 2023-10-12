950f2e92e6d4d72eb357ae740fa70b56e6427581
marco.veronesi <marco.veronesi@keyos.it>
2023-10-12 15:28:04 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::post('/scansioni', [ScansioniController::class, 'store'])->name('scansion
 Route::get('/scansioni/create', [ScansioniController::class, 'create'])->name('scansioni.create');
 Route::get('/scansioni/export', [ScansioniController::class, 'export'])->name('scansioni.export');
 /* Route::patch('/scansioni/{id}', [ScansioniController::class, 'update'])->name('scansioni.update'); */
-/* Route::get('/scansioni/{id}/edit', [ScansioniController::class, 'edit'])->name('scansioni.edit'); //TODO: */
+Route::get('/scansioni/{id}/edit', [ScansioniController::class, 'edit'])->name('scansioni.edit'); //TODO:
 Route::delete('/scansioni/{id}', [ScansioniController::class, 'destroy'])->name('scansioni.destroy');
